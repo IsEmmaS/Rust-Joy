fc31621629ce16bd9eee0b1319cbf985148ff882
LeeUbuntu <islijinchen@hotmail.com>
2024-09-07 21:00:00 +0800

    First learn.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rust write for myself
+
+## Basic type (Rust is a static language)
+
+- Scalar type : integer, floating-point complex
+  - Numeric Operations like + - * / %
+  - Bit Operation like & | ^ ! >> <<
+- String : &str
+- Boolean type : true and false
+- Character type : Single Unicode char
+- Compound type
+  - Tuple type : like (i32, u8, f64)
+  - Array type : like [1, 3, 8]
+- Can use "As" change from different type
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // Describe Value Type
+    const DATA_PATH :&str = "this/is/data/path"; // const in Rust have to add type describe
+    let data = 1; // var default is immutable
+    let mut variable_data = 1; // add mut to be a mutable var
+    let flag:bool = true;
+
+    let flag = 'A'; // create a new flag and shadowing the old flag
+    let _y = "a"; // add '_' in front of var name can ignore the compiler warn of "unused variable: y"
 }
