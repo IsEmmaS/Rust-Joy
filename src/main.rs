029cafa529315e2b1a09bf3c2185c3ae8c36bf56
LeeUbuntu <islijinchen@hotmail.com>
2024-09-21 11:11:31 +0800

    do some test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+fn add(a:i32, b:i32)->i32{
+    // rust function var type is essential
+    a + b
+}
+
+fn return_null(a:i32){
+    // This fn return a "()"
+    // or writen as
+    // fn return_null(a:i32)->()
+    // is also good
+}
+
+fn function_diverge()->!{
+    panic!("Never return!")
+    // diverge function never return anything
+    // always used as error expect
+}
+
+
 fn main() {
     // Describe Value Type
     const DATA_PATH :&str = "this/is/data/path"; // const in Rust have to add type describe
@@ -7,4 +26,6 @@ fn main() {
 
     let flag = 'A'; // create a new flag and shadowing the old flag
     let _y = "a"; // add '_' in front of var name can ignore the compiler warn of "unused variable: y"
+    let a = return_null(data);
+    println!("{:?}", a) // Add really return "()".
 }
